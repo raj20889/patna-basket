58169a8d728dcd769bddf83a9dd21baabef0ae3c
raj20889 <rajm20889@gmail.com>
2026-02-28 05:08:46 +0530

    fixed socket.io hardcodded value

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Replace with your server URL
+const socket = io(import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"); // Dynamically use the backend URL
 
 export default socket;
